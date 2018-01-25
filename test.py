8207a03e0a6d6461b582e69ac8ed8cde650a9fb4
Noah Mcaulay <noah.mcaulay@gmail.com>
2018-01-24 22:32:08 -0800

    Test?

--- test.py
+++ test.py
@@ -7,12 +7,6 @@ sys.path.append(os.path.join(HERE, "vendored"))
 
 import tensorflow as tf
 
-# change this as you see fit
-image_path = sys.argv[1]
-
-# Read in the image_data
-image_data = tf.gfile.FastGFile(image_path, 'rb').read()
-
 # Loads label file, strips off carriage return
 label_lines = [line.rstrip() for line
                in tf.gfile.GFile("retrained_labels.txt")]
@@ -23,17 +17,31 @@ with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
 
-with tf.Session() as sess:
-    # Feed the image_data as input to the graph and get first prediction
-    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+def predict(event, context):
 
-    predictions = sess.run(softmax_tensor, \
-                           {'DecodeJpeg/contents:0': image_data})
+    # change this as you see fit
+    image_path = sys.argv[1]
 
-    # Sort to show labels of first prediction in order of confidence
-    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    # Read in the image_data
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
-    for node_id in top_k:
-        human_string = label_lines[node_id]
-        score = predictions[0][node_id]
-        print('%s (score = %.5f)' % (human_string, score))
+    with tf.Session() as sess:
+        # Feed the image_data as input to the graph and get first prediction
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+        predictions = sess.run(softmax_tensor, \
+                               {'DecodeJpeg/contents:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+
+def main():
+    predict(0,0)
+
+if __name__ == "__main__":
+    main()
